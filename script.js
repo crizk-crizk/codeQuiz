0c7dc0d07b59e1541d48ef10b90e25690d3936f6
Carolina R <carolinar@carolinas-mbp-2.myfiosgateway.com>
2021-01-18 12:56:36 -0500

    added sbasic content to html

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+
+Create templates for HTML, JS, and CSS -- DONE!
+
+Start Button
+
+Create data structure that holds questions and asnwers
+
+Timer 
+
+Score display - data structure to keep track of highscores 
+
+Process to show questions and allow selection of answers
+
+End or Repeat Game button
+*/
+
+
+
+
+start
