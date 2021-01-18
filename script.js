c43cd3fe573c1cb179fee7ae12a496c639ce2dec
Carolina R <carolinar@carolinas-mbp-2.myfiosgateway.com>
2021-01-18 13:56:07 -0500

    added function start

--- script.js
+++ script.js
@@ -41,20 +41,43 @@ function start() {
     
     // loop to show the questions and answers
     var i = 0;
-    //for(i =0; i < questions.length; i++) {
-        var html = "";
-        var current_q = questions[i];
-        html += `<h2>${current_q.q}</h2>`;
-        // add the answers (as buttons) buttons need listener...
-        for(b = 0; b < current_q.a.length; b++) {
-            html += `<button>${current_q.a[b]}</button>`;
-        }
-        // display in the quiz div
-        document.getElementById('quiz').innerHTML = html;
 
-    //}
+}
 
+function check_answer(questionNumber, answerIndex) {
     
+    prompt(questionNumber + ' ' + answerIndex);
+
+    // For the incoming question number, see if the correct answerIndex was clicked.
+    var current_q = questions[questionNumber];
+
+    if (current_q.c === answerIndex) {
+
+        // Play sound
+        // Show correct
+
+    } else {
+        // play sound
+        // show "Wrong!"
+        // subtract some time from the timer....
+    }
+
+    // Proceed to and display the next question... (but was this the last question?)
+    showQuestion(questionNumber+1);
+    
+}
+
+function showQuestion(i) {
+
+    var html = "";
+    var current_q = questions[i];
+    html += `<h2>${current_q.q}</h2>`;
+    // add the answers (as buttons) buttons need listener...
+    for(b = 0; b < current_q.a.length; b++) {
+        html += `<button onclick="check_answer(${i},${b})">${current_q.a[b]}</button>`;
+    }
+    // display in the quiz div
+    document.getElementById('quiz').innerHTML = html;
 
 }
 
