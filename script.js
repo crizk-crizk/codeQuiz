22266280bfff08ea2dc092dd15f7b7a0109ec20a
Carolina R <carolinar@carolinas-mbp-2.myfiosgateway.com>
2021-01-18 13:37:12 -0500

    added data structure and initialize function

--- script.js
+++ script.js
@@ -15,7 +15,47 @@ Process to show questions and allow selection of answers
 End or Repeat Game button
 */
 
+// Data structure for Q&A
+// Question, possible answers, indication of correct answer
 
+var questions = [
+    {
+        q: 'What is your favorite color?',
+        a: ['Red','Blue','Yellow','Rainbow'],
+        c: 1
+    },
+    {
+        q: 'What is your favorite food',
+        a: ['Cake','Steak','Rice','Fish'],
+        c: 1
+    },
+];
 
+function initialize() {
 
-start
+    document.getElementById("quiz").innerHTML = "<button onclick='start();'>Start Code Quiz</button>";
+
+}
+
+function start() {
+    
+    // loop to show the questions and answers
+    var i = 0;
+    //for(i =0; i < questions.length; i++) {
+        var html = "";
+        var current_q = questions[i];
+        html += `<h2>${current_q.q}</h2>`;
+        // add the answers (as buttons) buttons need listener...
+        for(b = 0; b < current_q.a.length; b++) {
+            html += `<button>${current_q.a[b]}</button>`;
+        }
+        // display in the quiz div
+        document.getElementById('quiz').innerHTML = html;
+
+    //}
+
+    
+
+}
+
+initialize();
